d344634333613c6a18ccb6c607abd7fcfbdc5b7c
axun <cenjianxun08@gmail.com>
2024-01-17 15:36:19 +0800

    改rime

--- Rime/sync.sh
+++ Rime/sync.sh
@@ -2,18 +2,12 @@
 # bash sync.sh up 是local改了传到dotfiles
 # bash sync.sh down 是dotfiles给local
 
-PATH="/Users/liukun/Library/Rime"
+RIME_PATH="/Users/liukun/Library/Rime"
 
-if $0 == 'up'; then
-	cp $PATH/default.custom.yaml ./
-	cp $PATH/symbols.yaml ./
-	cp $PATH/squirrel.custom.yaml ./
-	cp $PATH/luna_pinyin.custom.yaml ./
+if [ "$1" == "up" ]; then
+	cp $RIME_PATH/*.yaml .
 fi
 
-if $0 == 'down'; then
-	cp default.custom.yaml $PATH/
-	cp symbols.yaml $PATH/
-	cp squirrel.custom.yaml $PATH/
-	cp luna_pinyin.custom.yaml $PATH/
+if [ "$1" == "down" ]; then
+	cp *.yaml $RIME_PATH/
 fi
