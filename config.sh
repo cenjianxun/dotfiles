c22eec8cb1bc3437e8aa27aaea6bb7b9debc2e89
xun <cenjianxun08@gmail.com>
2025-02-23 01:25:30 +0800

    fix:手动下载软件

--- config.sh
+++ config.sh
@@ -35,15 +35,17 @@ HISTFILESIZE=5000
 # install & alias
 #----------------------------------------
 
+# 手动下载软件！！！！！python要用brew下载的话不能使用pip！！！
+
 # 本来判断软件如果没安装就自动安装，结果不需要安装的地方每次都跳出来问要不要安装。还是全注释掉，没安装就不管，安装了再设置。
 
-if [[ `uname` == "Darwin" ]]; then
-	#check="brew list"
-	download="brew"
-else
-	#check="rpm -qa"
-	download="yum"
-fi
+# if [[ `uname` == "Darwin" ]]; then
+# 	#check="brew list"
+# 	download="brew"
+# else
+# 	#check="rpm -qa"
+# 	download="yum"
+# fi
 
 # 本来检查是否安装命令是 $check ｜ grep -qw python3, 不如下面这个通用，改了。
 
@@ -64,13 +66,13 @@ fi
 # fi
 
 # -w 判断这个path本用户有没有权限。因为lab的server咱没有root权限
-if command -v python3 &>/dev/null && [ ! -e /usr/local/bin/python ] && [ -w "/usr/local/bin" ]; then
-	# 因为nohup不能识别alias，所以这里只能用ln，将python3的路径给python。且只用运行一次。
-	# ln -s $(which python3) /usr/local/bin/python
-	# ln -s $(which pip3) /usr/local/bin/pip
-	alias python=python3
-	alias pip=pip3
-fi
+# if command -v python3 &>/dev/null && [ ! -e /usr/local/bin/python ] && [ -w "/usr/local/bin" ]; then
+# 	# 因为nohup不能识别alias，所以这里只能用ln，将python3的路径给python。且只用运行一次。
+# 	# ln -s $(which python3) /usr/local/bin/python
+# 	# ln -s $(which pip3) /usr/local/bin/pip
+# 	alias python=python3
+# 	alias pip=pip3
+# fi
 
 #alias python="/opt/homebrew/bin/python3.10"
 #alias pip="/opt/homebrew/bin/pip3.10"
@@ -89,18 +91,18 @@ fi
 # 	esac
 # fi
 
-if command -v go &>/dev/null; then
-	go env -w GO111MODULE=on
-	export GOPROXY=https://goproxy.cn,direct
+# if command -v go &>/dev/null; then
+# 	go env -w GO111MODULE=on
+# 	export GOPROXY=https://goproxy.cn,direct
 	
-	GOROOT=$(go env GOROOT)
-	export GOPATH="$(brew --prefix go)"
-	export GOBIN="${GOROOT}/bin"
+# 	GOROOT=$(go env GOROOT)
+# 	export GOPATH="$(brew --prefix go)"
+# 	export GOBIN="${GOROOT}/bin"
 
-	if [[ ":${PATH}:" != *"${GOBIN}"* ]]; then
-		export PATH="$PATH:${GOBIN}"
-	fi
-	if [[ ":${PATH}:" != *"${GOPATH}/bin"* ]]; then
-		export PATH="$PATH:${GOPATH}/bin"
-	fi
-fi
+# 	if [[ ":${PATH}:" != *"${GOBIN}"* ]]; then
+# 		export PATH="$PATH:${GOBIN}"
+# 	fi
+# 	if [[ ":${PATH}:" != *"${GOPATH}/bin"* ]]; then
+# 		export PATH="$PATH:${GOPATH}/bin"
+# 	fi
+# fi
